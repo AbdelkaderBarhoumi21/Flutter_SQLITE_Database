3093508f594ba1257f91fec56c57b972d87a1a74
AbdelkaderBarhoumi21 <abdelkaderbarhoumi21@gmail.com>
2026-03-27 21:29:08 +0100

    feat: add note repository

---
+++ lib/data/repository/note_repository.dart
@@ -0,0 +1,47 @@
+import 'package:flutter_sqlite_database/data/daos/note_daos.dart';
+import 'package:flutter_sqlite_database/data/db/app_database.dart';
+
+class NoteRepository {
+  NoteRepository(this.noteDao);
+  NoteDao noteDao;
+
+  /// Get all notes from the database
+  Future<List<NoteTableData>> getAllNotes() async {
+    return await noteDao.getAllNotes();
+  }
+
+  /// Watch all notes with real-time updates
+  Stream<List<NoteTableData>> watchAllNotes() {
+    return noteDao.watchAllNotes();
+  }
+
+  /// Insert a new note into the database
+  Future<int> insertNote(NoteTableCompanion note) async {
+    return await noteDao.insertNote(note);
+  }
+
+  /// Get a specific note by its ID
+  Future<NoteTableData?> getNoteById(int id) async {
+    return await noteDao.getNoteById(id);
+  }
+
+  /// Update an existing note
+  Future<bool> updateNote(NoteTableCompanion note) async {
+    return noteDao.updateNote(note);
+  }
+
+  /// Delete a note by its ID
+  Future<int> deleteNote(int id) async {
+    return noteDao.deleteNote(id);
+  }
+
+  /// Delete all notes from the database
+  Future<int> deleteAllNotes() async {
+    return noteDao.deleteAllNotes();
+  }
+
+  /// Search notes by query string
+  Stream<List<NoteTableData>> searchNotes(String query) {
+    return noteDao.searchNotes(query);
+  }
+}
