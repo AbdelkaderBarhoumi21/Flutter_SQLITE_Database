0f2f3bf698c64ca4f9e87cfb8b2b5ef13b059b7a
AbdelkaderBarhoumi21 <abdelkaderbarhoumi21@gmail.com>
2026-03-28 17:01:18 +0100

    Fix: remove debug check mode banneer

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_sqlite_database/ui/view/widgets/note_list_view.dart';
 
 void main() {
-  runApp(
-    const ProviderScope(
-      child: MyApp(),
-    ),
-  );
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -16,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Note app',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
