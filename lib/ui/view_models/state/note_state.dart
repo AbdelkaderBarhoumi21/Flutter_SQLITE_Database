363eb885234bda3f8531f81de824283a73946ff4
AbdelkaderBarhoumi21 <abdelkaderbarhoumi21@gmail.com>
2026-03-28 13:29:10 +0100

    feat: add note state class

---
+++ lib/ui/view_models/state/note_state.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_sqlite_database/domain/models/note_model.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'note_state.freezed.dart';
+
+@freezed
+abstract class NoteState with _$NoteState {
+  factory NoteState({
+    @Default(false) bool isLoading,
+    @Default([]) List<NoteModel> notes,
+    String? error,
+  }) = _NoteState;
+}
