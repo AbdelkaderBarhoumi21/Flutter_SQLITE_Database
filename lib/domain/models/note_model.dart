47c81ccff334b9054e3e56e1a855b142d019043a
AbdelkaderBarhoumi21 <abdelkaderbarhoumi21@gmail.com>
2026-03-28 12:36:00 +0100

    feat: add note model

---
+++ lib/domain/models/note_model.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_sqlite_database/data/db/app_database.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'note_model.freezed.dart';
+
+@freezed
+abstract class NoteModel with _$NoteModel {
+  factory NoteModel({
+    required int id,
+    required String title,
+    String? description,
+    required String createdAt,
+  }) = _NoteModel;
+
+  factory NoteModel.fromEntity(NoteTableData note) => NoteModel(
+    id: note.id,
+    title: note.title,
+    description: note.description,
+    createdAt: formatDateTimeDifference(note.createdAt),
+  );
+}
+
+String formatDateTimeDifference(DateTime date) {
+  final now = DateTime.now();
+  final dif = now.difference(date);
+
+  if (dif.inDays > 0) {
+    return '${dif.inDays} days ago';
+  } else if (dif.inHours > 0) {
+    return '${dif.inHours} hours ago';
+  } else if (dif.inMinutes > 0) {
+    return '${dif.inMinutes} minutes ago';
+  } else {
+    return 'Just now';
+  }
+}
