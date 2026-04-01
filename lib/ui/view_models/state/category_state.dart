01e09a8ca69ed3e2c20ffe348c99d19368655f63
AbdelkaderBarhoumi21 <abdelkaderbarhoumi21@gmail.com>
2026-04-01 16:00:50 +0100

    feat: add category state

---
+++ lib/ui/view_models/state/category_state.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_sqlite_database/domain/models/category_model.dart';
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'category_state.freezed.dart';
+
+@freezed
+abstract class CategoryState with _$CategoryState{
+  factory CategoryState({
+    @Default(false) bool isLoading,
+    @Default(false) bool isUpdated,
+    @Default(false) bool isDeleted,
+    @Default([]) List<CategoryModel> categories,
+    CategoryModel? category,
+    String? error,
+  }) = _CategoryState;
+}
